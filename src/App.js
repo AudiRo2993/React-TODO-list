eba1dda7195851d8bb7ec61469d0676ba35d846f
AudiRo <audiro.personal@gmail.com>
2023-05-22 17:11:40 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import Todo from "./components/Todo";
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+     <Todo text="123" />
+     <Todo text="Master React"/>
+     <Todo text="Explore the full react course"/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+
+function Todo(props) {
+const [modalIsOpen, setModalIsOpen] = useState(false);
+
+function closeModalHandler() {
+    setModalIsOpen(false);
+}
+
+function deleteHandler() {
+   setModalIsOpen(true);
+}
+
+    return (
+       
+            <div className="card">
+            <h2>{props.text}</h2>
+            <div className="actions">
+            <button className="btn" onClick={deleteHandler}>Delete</button>
+            </div>
+            { modalIsOpen ? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> : null}
+            { modalIsOpen ? <Backdrop onClick={closeModalHandler}/> : null}
+           
+          </div>
+        
+    )
+}
+
+export default Todo;
